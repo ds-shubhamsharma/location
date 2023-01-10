9248aef617eaf8778cfaeabc66c9600e34ef33f5
shubhamdots <shubhamsharma0888@gmail.com>
2023-01-10 12:14:07 +0530

    update

--- src/templates/location.tsx
+++ src/templates/location.tsx
@@ -8,7 +8,6 @@ import {
   TemplateProps,
   TemplateRenderProps,
 } from "@yext/pages";
-import { Link } from "@yext/pages/components";
 import * as React from "react";
 import Footer from "../components/footer";
 import Header from "../components/header";
@@ -17,6 +16,7 @@ import "../index.css";
 export const config: TemplateConfig = {
   stream: {
     $id: "location-1",
+
     fields: [
       "id",
       "uid",
@@ -26,7 +26,7 @@ export const config: TemplateConfig = {
       "description",
       "color",
       "photoGallery",
-      "slug",
+      "slug"
     ],
 
     filter: {
@@ -86,55 +86,29 @@ const Location: Template<TemplateRenderProps> = ({ document }) => {
   return (
     <>
       <Header />
-
-      <div className="container-fluid">
-        <div className="row ">
-          <div className="col-5" id="product_image">
-            <img src={images} alt="sdf" style={styl} />
-          </div>
-          <div className=" col-lg-5 mt-5">
-            <h3> {name}</h3>
-            <hr />
-            <p id="product_price">{price.value}</p>
-            
-            <p id="product_price">{color}</p>
-            <hr />
-            <h4>Description:</h4>
-            <p>{description}</p>
-            <hr />
+      <div>
+        <div className="centered-container">
+          <div className="section">
+            <div className="grid grid-cols-2 gap-x-10 gap-y-10">
+              <div
+                className="bg-gray-100 p-2"
+                style={{ color: "black", fontFamily: "cursive" }}
+              >{`product name :  ${name}`}</div>
+              <div className="bg-gray-100 p-2">
+                <p>{`price :    $${document?.price.value}`}</p>
+              </div>
+              <div className="bg-gray-100 p-2">
+                <div className="text-xl font-semibold">{`About ${name}`}</div>
+                <p className="pt-4">{description}</p>
+              </div>
+              <div className="bg-gray-100">{images}</div>
+            </div>
           </div>
         </div>
       </div>
-
-      <Footer />
+      <Footer/>
     </>
   );
 };
 
 export default Location;
-
-const styl = {
-  height: 500,
-  width: 500,
-};
-
-{
-  /* <div className="centered-container">
-<div className="section">
-  <div className="grid grid-cols-2 gap-x-10 gap-y-10">
-    <div
-      className="bg-gray-100 p-2"
-      style={{ color: "black", fontFamily: "cursive" }}
-    >{`product name :  ${name}`}</div>
-    <div className="bg-gray-100 p-2">
-      <p>{`price :    $${document?.price.value}`}</p>
-    </div>
-    <div className="bg-gray-100 p-2">
-      <div className="text-xl font-semibold">{`About ${name}`}</div>
-      <p className="pt-4">{description}</p>
-    </div>
-    <div className="bg-gray-100">{images}</div>
-  </div>
-</div>
-</div> */
-}
